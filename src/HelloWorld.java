09f92ebfaa1205cf2df6e5a34a671f1ebf72f53d
Abdullah Kural <portreciziyorum@gmail.com>
2022-03-31 12:06:15 +0200

    work3 is done!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 
     //Doing awesome work2
 
+    //Doing great work 3!
+    //Great work3 is done and ready to be committed
+    //and ready to be pushed to remote repository
+
 }
 
 
